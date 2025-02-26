4f8fff3425f53b7e0ece2be702990d754dbcdb93
Mark Kendall <mkendall.tutor@gmail.com>
2025-02-25 23:15:52 -0600

    Configure Merchant and Item serializer

--- spec/requests/api/v1/items_controller_spec.rb
+++ spec/requests/api/v1/items_controller_spec.rb
@@ -2,7 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Items Controller tests", type: :requests do
   describe "Updating (patch) tests" do
+    it "can update a Merchant record with only name provided" do
 
+    end
+
+    it "correcltly ignore attributes beyond name in updating" do
+
+    end
+
+    it "handles empty body request properly" do
+
+    end
+
+    it "sends appropriate 400 level error when no id found" do
+
+    end
 
   end
 

7a7d539525aa3e6c1ae4e85aca7349cb991989ee
Mark Kendall <mkendall.tutor@gmail.com>
2025-02-26 17:41:52 -0600

    Merge fail recovery: update merchants_controller.rb

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::MerchantsController < ApplicationController
+  def index
+    if params[:sorted] == "age"
+      merchants = Merchant.sorted_by_age
+    else
+      merchants = Merchant.all
+    end
+    
+    if params[:status] == "returned"
+      merchants = Merchant.has_returned_items
+    end
 
+    #also need returned and count values
+
+    render json: MerchantSerializer.new(merchants)
+  end
+  
   def update
     updated_merchant = Merchant.update(params[:id], merchant_update_params)
 

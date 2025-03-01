5e58aac1fda380012d92b2ef43f3f4223fe33a85
Mark Kendall <mkendall.tutor@gmail.com>
2025-02-28 23:35:12 -0600

    Add Items/MerchantsController#index and associated happy/sad path tests

---
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::Items::MerchantController < ApplicationController
+  #This specifically controls the /api/v1/items/:id/merchant endpoint
+
+  def index
+    begin
+      specified_item = Item.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => error
+
+      # binding.pry
+
+      render json: {
+        message: "Your request could not be completed.",
+        errors: [ { message: error.message } ]
+      }, status: 404
+
+      return
+    end
+
+    begin
+      associated_merchant = specified_item.merchant
+    rescue ActiveRecord::RecordNotFound => error
+      render json: {
+        message: "Your request could not be completed.",
+        errors: [ { message: error.message } ]
+      }, status: 404
+
+      return
+    end
+
+    render json: MerchantSerializer.new(associated_merchant)
+    
+  end
+end
---
+++ spec/requests/api/v1/item/merchants_request_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe "Merchant (of Item) endpoints", type: :request do
+  
+  before(:each) do
+    #Added one item for additional sad path logic
+    
+    Merchant.destroy_all
+    @merchant1 = Merchant.create!(name: "Barbara")
+    @merchant2 = Merchant.create!(name: "Mark")
+    @merchant3 = Merchant.create!(name: "Jackson")
+    @merchant4 = Merchant.create!(name: "Jason")
+    
+    Item.destroy_all
+    @item1 = Item.create!(name: "Cat toy", description: "wiggling fish", unit_price: 0.34, merchant_id: @merchant1[:id])
+    @item2 = Item.create!(name: "orange cream soda", description: "tasty and citrusy", unit_price: 3, merchant_id: @merchant2[:id])
+    @item3 = Item.create!(name: "root beer", description: "smooth saspirilla", unit_price: 2, merchant_id: @merchant2[:id])
+    @item4 = Item.create!(name: "can of ground peas", description: "mush", unit_price: 5, merchant_id: @merchant3[:id])
+    @item5 = Item.create!(name: "cube", description: "not just any rectangular prism", unit_price: 8.00, merchant_id: @merchant4[:id])
+    @item6 = Item.create!(name: "sphere", description: "now if only it were a cow", unit_price: 512.00, merchant_id: @merchant4[:id])
+    @item7 = Item.create!(name: "an unassuming book", description: "actually connects to Riven", unit_price: 1996.00, merchant_id: @merchant2[:id])   #For sad path logic
+  end
+
+  describe "#index tests" do
+    it "happy path: returns merchant belonging to specified item (two examples)" do
+      #Try item belonging to @merchant2 (has another item)
+      get "/api/v1/items/#{@item3.id}/merchant"
+
+
+
+      #Try item belonging to @merchant3
+
+    end
+    
+    it "sad path: item does not exist" do
+      nonexistant_id = 100000
+      get "/api/v1/items/#{nonexistant_id}/merchant"
+      error_message = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      #Related: could I group some of these (DRY)?  I suspect so...
+      expect(error_message[:message]).to eq("Your request could not be completed.")
+      expect(error_message[:errors]).to be_a(Array)
+      expect(error_message[:errors].first[:message]).to eq("Couldn't find Item with 'id'=#{nonexistant_id}")
+
+    end
+
+    it "sad path: item has no associated merchant" do
+      #This one is harder to test, since creating an item requies a legal merchant_id
+      #Further, the DB is protecting against this.  I could override in the model, but this is probably not needed for this project...
+
+      # get "/api/v1/items/#{@item7.id}/merchant"
+
+      # expect(response).to_not be_successful
+      # expect(response.status).to eq(404)
+      # #Related: could I group some of these (DRY)?  I suspect so...
+      # expect(error_message[:message]).to eq("Your request could not be completed.")
+      # expect(error_message[:errors]).to be_a(Array)
+      # expect(error_message[:errors].first[:message]).to eq("Couldn't find Merchant with 'id'=#{nonexistant_id}")
+    end
+
+  end
+
+end

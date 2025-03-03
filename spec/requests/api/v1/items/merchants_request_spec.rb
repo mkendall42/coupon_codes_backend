14340b5639e8250c033a78e083c4666c4b5140a9
Mark Kendall <mkendall.tutor@gmail.com>
2025-03-02 19:05:56 -0600

    Update merchants_request_spec.rb tests and directory structure

--- spec/requests/api/v1/items/merchants_request_spec.rb
+++ spec/requests/api/v1/items/merchants_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Merchant (of Item) endpoints", type: :request do
   
   before(:each) do
-    #Use the same initial dataset as for parent Merchant, Item controllers
+    #Added one item for additional sad path logic
     
     Merchant.destroy_all
     @merchant1 = Merchant.create!(name: "Barbara")
@@ -18,9 +18,47 @@ RSpec.describe "Merchant (of Item) endpoints", type: :request do
     @item4 = Item.create!(name: "can of ground peas", description: "mush", unit_price: 5, merchant_id: @merchant3[:id])
     @item5 = Item.create!(name: "cube", description: "not just any rectangular prism", unit_price: 8.00, merchant_id: @merchant4[:id])
     @item6 = Item.create!(name: "sphere", description: "now if only it were a cow", unit_price: 512.00, merchant_id: @merchant4[:id])
+    @item7 = Item.create!(name: "an unassuming book", description: "actually connects to Riven", unit_price: 1996.00, merchant_id: @merchant2[:id])   #For potential sad path logic
   end
 
   describe "#index tests" do
+    it "happy path: returns merchant belonging to specified item (two examples)" do
+      #Try item belonging to @merchant2 (has another item)
+      get "/api/v1/items/#{@item3.id}/merchant"
+
+
+
+      #Try item belonging to @merchant3
+
+    end
+    
+    it "sad path: item does not exist" do
+      nonexistant_id = 100000
+      get "/api/v1/items/#{nonexistant_id}/merchant"
+      error_message = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      #Related: could I group some of these (DRY)?  I suspect so...
+      expect(error_message[:message]).to eq("Your request could not be completed.")
+      expect(error_message[:errors]).to be_a(Array)
+      expect(error_message[:errors].first[:message]).to eq("Couldn't find Item with 'id'=#{nonexistant_id}")
+
+    end
+
+    it "sad path: item has no associated merchant" do
+      #This one is harder to test, since creating an item requies a legal merchant_id
+      #Further, the DB is protecting against this.  I could override in the model, but this is probably not needed for this project...
+
+      # get "/api/v1/items/#{@item7.id}/merchant"
+
+      # expect(response).to_not be_successful
+      # expect(response.status).to eq(404)
+      # #Related: could I group some of these (DRY)?  I suspect so...
+      # expect(error_message[:message]).to eq("Your request could not be completed.")
+      # expect(error_message[:errors]).to be_a(Array)
+      # expect(error_message[:errors].first[:message]).to eq("Couldn't find Merchant with 'id'=#{nonexistant_id}")
+    end
 
   end
 

a1c7d2e8aa46b142280e3edb2161ed460dc0f46f
Jacob Inman <jrinman6295@gmail.com>
2025-03-05 09:30:42 -0800

    Invoice model spec added

--- spec/model/invoice_spec.rb
+++ spec/model/invoice_spec.rb
@@ -7,4 +7,39 @@ describe Invoice, type: :model do
     it { should have_many :invoice_items }
     it { should have_many :transactions }
   end
+
+  describe "filter by status(status)" do
+    before(:each) do
+      # Merchant.destroy_all
+      @merchant1 = Merchant.create!(name: "Barbara")
+      @merchant2 = Merchant.create!(name: "Mark")
+      @merchant3 = Merchant.create!(name: "Jackson")
+      @merchant4 = Merchant.create!(name: "Jason")
+  
+      # Customer.destroy_all
+      @customer1 = Customer.create!(first_name: "John J.", last_name: "Jingleheimerschmidt")
+      @customer2 = Customer.create!(first_name: "Timmy", last_name: "Turner")
+      @customer3 = Customer.create!(first_name: "Spongebob", last_name: "Squarepants")
+  
+      # Invoice.destroy_all
+      @invoice1 = Invoice.create!(customer_id: @customer1.id, merchant_id: @merchant1.id, status: "shipped")
+      @invoice2 = Invoice.create!(customer_id: @customer1.id, merchant_id: @merchant1.id, status: "returned")
+      @invoice3 = Invoice.create!(customer_id: @customer2.id, merchant_id: @merchant2.id, status: "shipped")
+      @invoice4 = Invoice.create!(customer_id: @customer3.id, merchant_id: @merchant2.id, status: "shipped")
+    end
+
+    describe "can filter by a given shipping status" do
+      it "for shipped" do
+        invoices = [@invoice1, @invoice2, @invoice3, @invoice4]
+
+        expect(Invoice.filter_by_status("shipped").count).to eq(3)
+      end
+  
+      it "for returned" do
+        invoices = [@invoice1, @invoice2, @invoice3, @invoice4]
+
+        expect(Invoice.filter_by_status("returned").count).to eq(1)
+      end
+    end
+  end
 end
